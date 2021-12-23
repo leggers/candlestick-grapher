ebcd70f652415346fb08cc17b8987eab19eca447
Lucas S Eggers <l.eggers91@gmail.com>
2021-12-23 11:57:26 -0800

    update graph to include tick and vwap data. also adjust bar position to (hopefully?) make more sense

--- candlestick.py
+++ candlestick.py
@@ -2,9 +2,14 @@ import streamlit as st
 import plotly.graph_objects as go
 import plotly.subplots as sp
 import pandas as pd
+from datetime import timedelta
+import json
 
 import re
 from pathlib import Path
+
+################################################################################
+# Date and ticker selector
 
 ORDERS_RE = re.compile("orders")
 CSV_NAME_RE = re.compile("([A-Z]+)_*\d+_S.csv")
@@ -21,8 +26,33 @@ for date_folder in date_folders:
 data_date = st.selectbox("Data date:", dates_available)
 symbol = st.selectbox("Symbol:", dates_to_tickers[data_date])
 
-data = pd.read_csv(f"~/Documents/stonk_data/{data_date}/{symbol}_60_S.csv")
+################################################################################
+# Read data
+
+one_min_data = pd.read_csv(f"~/Documents/stonk_data/{data_date}/{symbol}_60_S.csv")
+one_minute = timedelta(minutes=1)
+# this will cause the 5 minute bar to overlap with the _last_ one minute bar it
+# contains, instead of the first, which is confusing to look at.
+one_min_data['adjusted_time'] = pd.to_datetime(one_min_data['time']) + one_minute
+
 five_min_data = pd.read_csv(f"~/Documents/stonk_data/{data_date}/{symbol}_300_S.csv")
+five_and_a_half_minutes = timedelta(minutes=5, seconds=30)
+# this will cause the 5 minute bar to overlap with the _last_ one minute bar it
+# contains, instead of the first, which is confusing to look at.
+five_min_data['adjusted_time'] = pd.to_datetime(five_min_data['time']) + five_and_a_half_minutes
+
+tick_data_list = []
+with open(f"/Users/leggers/Documents/stonk_data/{data_date}/{symbol}_ticks.json") as f:
+    for line in f:
+        tick_data_list.append(json.loads(line))
+
+tick_data = pd.DataFrame(tick_data_list)
+tick_data['price'] = list(map(lambda t: float(t.split(';')[0]), tick_data['tick']))
+tick_data['vwap'] = list(map(lambda t: float(t.split(';')[4]), tick_data['tick']))
+tick_data['time'] = pd.to_datetime(tick_data['recordedAt'])
+
+################################################################################
+# Draw plots
 
 fig = sp.make_subplots(
   rows=2, cols=1,
@@ -30,34 +60,64 @@ fig = sp.make_subplots(
   row_width=[0.2, 0.7]
 )
 
+# one minute data
 fig.add_trace(go.Candlestick(
-                  x=five_min_data['time'],
+                x=one_min_data['adjusted_time'],
+                open=one_min_data['open'],
+                high=one_min_data['high'],
+                low=one_min_data['low'],
+                close=one_min_data['close'],
+                name=symbol + ' 1m'),
+              row=1,
+              col=1)
+
+# five minute data
+fig.add_trace(go.Candlestick(
+                  x=five_min_data['adjusted_time'],
                   open=five_min_data['open'],
                   high=five_min_data['high'],
                   low=five_min_data['low'],
                   close=five_min_data['close'],
                   name=symbol + ' 5m',
-                  increasing_line_color= 'cyan', decreasing_line_color= 'orange'),
+                  ),
                 row=1,
                 col=1)
 
-# fig.add_trace(go.Bar(x=five_min_data['time'], y=five_min_data['volume'], showlegend=False),
-#                row=2,
-#                col=1)
+# tick data
+fig.add_trace(go.Scatter(
+  x=tick_data['time'],
+  y=tick_data['price'],
+  line=go.scatter.Line(color='black', width=0.3),
+  name=f"{symbol} ticks"),
+  row=1,
+  col=1)
 
-fig.add_trace(go.Candlestick(
-                x=data['time'],
-                open=data['open'],
-                high=data['high'],
-                low=data['low'],
-                close=data['close'],
-                name=symbol + ' 1m'),
-              row=1,
-              col=1)
+# VWAP
+fig.add_trace(go.Scatter(
+  x=tick_data['time'],
+  y=tick_data['vwap'],
+  line=go.scatter.Line(color='orange', width=0.3),
+  name=f"{symbol} reported VWAP"),
+  row=1,
+  col=1)
 
-fig.add_trace(go.Bar(x=data['time'], y=data['volume'], showlegend=False),
-               row=2,
-               col=1)
+# Candle-computed VWAP
+fig.add_trace(go.Scatter(
+  x=one_min_data['adjusted_time'],
+  y=one_min_data['vwap'],
+  line=go.scatter.Line(color='green', width=0.3),
+  name=f"{symbol} computed VWAP"),
+  row=1,
+  col=1)
+
+# One minute volume
+fig.add_trace(go.Bar(
+    x=one_min_data['adjusted_time'],
+    y=one_min_data['volume'],
+    showlegend=False,
+    name = f"{symbol} ticks"),
+  row=2,
+  col=1)
 
 fig.update_layout(
     title=f"{symbol} Price and Volume on {data_date}",
@@ -68,6 +128,7 @@ fig.update_layout(
         color="black"
     ),
     xaxis_rangeslider_visible=False,
+    xaxis_showspikes=True,
     shapes = [dict(
         x0=f"{data_date}T09:29:30", x1=f"{data_date}T09:29:30", y0=0, y1=1, xref='x1', yref='paper',
         line_width=2)],
